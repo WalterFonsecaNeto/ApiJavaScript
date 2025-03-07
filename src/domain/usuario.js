6cb64ac1bb216b36cf37542578cb04418e6c8653
Walter Fonseca <walterfonseca1606@gmail.com>
2025-03-07 09:07:20 -0300

    Arrumando crud do usuarios

--- src/domain/usuario.js
+++ src/domain/usuario.js
@@ -1,13 +1,18 @@
-import bcrypt from "bcrypt";
+const TipoUsuario = Object.freeze({
+  ADMIN: 1,
+  GERENTE: 2,
+  USUARIO: 3,
+});
 
 export class Usuario {
-  constructor({ 
-    id, 
-    nome, 
-    email, 
-    senha, 
-    status = true, 
-    dataCriacao = new Date(new Date().getTime() - (3 * 60 * 60 * 1000))
+  constructor({
+    id,
+    nome,
+    email,
+    senha,
+    status = true,
+    dataCriacao = new Date(new Date().getTime() - 3 * 60 * 60 * 1000),
+    tipoUsuario,
   }) {
     this.id = id;
     this.nome = nome;
@@ -15,6 +20,7 @@ export class Usuario {
     this.senha = senha;
     this.status = status;
     this.dataCriacao = dataCriacao;
+    this.tipoUsuario = tipoUsuario;
   }
 
   validarDados() {
@@ -27,9 +33,22 @@ export class Usuario {
     if (this.senha.length < 6) {
       throw new Error("Senha deve ter pelo menos 6 caracteres.");
     }
+
+    // Verificar se tipoUsuario é válido, ou seja, se é um número válido entre 1, 2 ou 3
+    if (
+      ![TipoUsuario.ADMIN, TipoUsuario.GERENTE, TipoUsuario.USUARIO].includes(
+        this.tipoUsuario
+      )
+    ) {
+      throw new Error("Tipo de usuário inválido.");
+    }
   }
-  
-  
 
-
+  // Método para exibir o tipo de usuário como string (ADMIN, GERENTE, USUARIO)
+  obterTipoUsuario() {
+    // Retorna o nome do tipo de usuário baseado no número
+    return Object.keys(TipoUsuario).find(
+      (key) => TipoUsuario[key] === this.tipoUsuario
+    );
+  }
 }
